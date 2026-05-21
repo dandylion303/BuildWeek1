97c43a9b7549c410c80162fe3f19f278b8ff62c0
Antonio-Puca <antoniopuca.work@gmail.com>
2026-05-21 16:36:05 +0200

    aggiungo results.js e results.html

---
+++ results.js
@@ -0,0 +1,8 @@
+// 1. LEGGO I DATI DALL'URL
+const params = new URLSearchParams(window.location.search);
+const score = params.get("score");
+const total = params.get("total");
+
+// 2. MOSTRO I DATI NELLA PAGINA
+document.getElementById("score").innerText = score;
+document.getElementById("total").innerText = total;
