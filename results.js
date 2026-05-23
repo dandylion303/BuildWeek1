46f536620f39ef6d15a90fd9572f301bc6f85c29
jacopoposer <jacoporagazzo@gmail.com>
2026-05-23 12:18:05 +0200

    css valutazione finale e centratura pagina

--- results.js
+++ results.js
@@ -8,15 +8,29 @@ document.getElementById("score").innerText = score;
 document.getElementById("total").innerText = total;
 // 3 MOSTRO STELLINE ALLA FINE DEL DOCUMENTO
 
-for (let i=0; i<total; i++){
+for (let i = 0; i < total; i++) {
     const star = document.createElement("img")
     star.setAttribute("src", "./assets/star.svg")
-    if (i<score) {
-         star.setAttribute("class", "correctAnswers")
-         
+    if (i < score) {
+        star.setAttribute("class", "correctAnswers")
+
     } else {
         star.setAttribute("class", "incorrectAnswers")
     }
 
     stellina.appendChild(star)
 }
+
+const evaluation = document.createElement("p")
+const main = document.getElementById("mainResults")
+evaluation.setAttribute("class","counter")
+
+if (score >= 6) {
+    evaluation.innerText = ("Exam Passed")
+    evaluation.setAttribute("style","color: green")
+} else {
+    evaluation.innerText = ("Exam Failed")
+    evaluation.setAttribute("style","color: red")
+}
+
+main.appendChild(evaluation)
